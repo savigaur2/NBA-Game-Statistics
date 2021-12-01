a7ba3ace59f7940f805f7d6637b122b87996474f
Savishwa Gaur <savishwa@Savishwas-MacBook-Pro.local>
2021-11-30 20:02:57 -0600

    caching chanegs

--- conf_teams.py
+++ conf_teams.py
@@ -3,7 +3,6 @@ import pandas as pd
 import numpy as np
 import mysql.connector
 
-@st.cache(allow_output_mutation=True, hash_funcs={"_thread.RLock": lambda _: None})
 def init_connection():
     return mysql.connector.connect(user = st.secrets['user'], database = st.secrets['database'], password = st.secrets['password'])
 
@@ -11,8 +10,6 @@ conn = init_connection()
 
 # Perform query.
 # Uses st.cache to only rerun when the query changes or after 10 min.
-@st.cache(ttl=600)
-
 def run_query(query):
     with conn.cursor() as cur:
         cur.execute(query)
--- initial_view.py
+++ initial_view.py
@@ -3,15 +3,13 @@ import pandas as pd
 import numpy as np
 import mysql.connector
 
-@st.cache(allow_output_mutation=True, hash_funcs={"_thread.RLock": lambda _: None})
 def init_connection():
     return mysql.connector.connect(user = st.secrets['user'], database = st.secrets['database'], password = st.secrets['password'])
 
 conn = init_connection()
 
 # Perform query.
-# Uses st.cache to only rerun when the query changes or after 10 min.
-@st.cache(ttl=600)
+
 
 def run_query(query):
     with conn.cursor() as cur:
