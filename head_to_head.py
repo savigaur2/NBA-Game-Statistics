8d6d96f67cd19b4b7c3cb5865c941344ac1f7534
Savishwa Gaur <savishwa@Savishwas-MacBook-Pro.local>
2021-11-30 19:53:11 -0600

    sql connection updates

--- head_to_head.py
+++ head_to_head.py
@@ -2,10 +2,19 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import mysql.connector
-cnx = mysql.connector.connect(**st.secrets["mysql"])
+
+@st.cache(allow_output_mutation=True, hash_funcs={"_thread.RLock": lambda _: None})
+def init_connection():
+    return mysql.connector.connect(**st.secrets["mysql"])
+
+conn = init_connection()
+
+# Perform query.
+# Uses st.cache to only rerun when the query changes or after 10 min.
+@st.cache(ttl=600)
 
 def run_query(query):
-    with cnx.cursor() as cur:
+    with conn.cursor() as cur:
         cur.execute(query)
         return cur.fetchall()
 
